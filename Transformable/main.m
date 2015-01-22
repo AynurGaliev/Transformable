061192d2647121ddede29238167908ee72676f4e
Aynur Galiev <aynur.galiev@flatstack.com>
2015-01-22 11:56:01 +0300

    Initial Commit

---
+++ Transformable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Transformable
+//
+//  Created by Aynur Galiev on 22.01.15.
+//  Copyright (c) 2015 Flatstack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
